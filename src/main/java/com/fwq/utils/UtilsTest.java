00776ab7ee141acb4b31bc7ad2042e5039483c3b
fanweiqi <sherlock_cxy@163.com>
2016-01-31 19:56:25 +0800

    spring test

--- src/main/java/com/fwq/utils/UtilsTest.java
+++ src/main/java/com/fwq/utils/UtilsTest.java
@@ -1,16 +1,11 @@
 package com.fwq.utils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import org.junit.Test;
-
 public class UtilsTest {
 
 	
-	@Test
 	public void testSendPost(){
+		System.out.println(System.getProperty("java.ext.dirs"));
+		
 		String url="http://www.sf-express.com/sf-service-web/service/bills/976455293179/routes?app=bill&lang=sc&region=cn&translate=";
 		String sendGet = Utils.sendHttp(url,"get");
 		
